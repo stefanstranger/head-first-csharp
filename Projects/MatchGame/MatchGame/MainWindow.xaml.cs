e779f888ec879b0f6d82f6b139711de3e92509ec
stefanstranger <stefan@stranger.nl>
2021-12-29 11:26:44 +0100

    Added mousedown method

--- Projects/MatchGame/MatchGame/MainWindow.xaml.cs
+++ Projects/MatchGame/MatchGame/MainWindow.xaml.cs
@@ -51,5 +51,30 @@ namespace MatchGame
                 animalEmoji.RemoveAt(index);
             }
         }
+
+        // variables outside of the methods
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock; if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+
+        }
     }
 }
