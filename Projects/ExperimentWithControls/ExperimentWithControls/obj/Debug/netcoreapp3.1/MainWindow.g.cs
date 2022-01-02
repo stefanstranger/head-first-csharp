de12b0de61f6d5109e8ad0b6e4f56b50739f1739
stefanstranger <stefan@stranger.nl>
2022-01-02 18:17:07 +0100

    let's add

--- Projects/ExperimentWithControls/ExperimentWithControls/obj/Debug/netcoreapp3.1/MainWindow.g.cs
+++ Projects/ExperimentWithControls/ExperimentWithControls/obj/Debug/netcoreapp3.1/MainWindow.g.cs
@@ -109,7 +109,7 @@ namespace ExperimentWithControls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ExperimentWithControls;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ExperimentWithControls;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
