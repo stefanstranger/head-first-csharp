e49e12124a1323013b53e416cdb6184845ec1e8c
stefanstranger <stefan@stranger.nl>
2022-01-08 15:13:24 +0100

    Further filtering!

--- Projects/Faker/Faker/Program.cs
+++ Projects/Faker/Faker/Program.cs
@@ -24,15 +24,36 @@ namespace FakerDemo
             User user = new User();
             user.FullName = Faker.Name.FullName();
             // Check if FullName contains Prof, Dr. Mr. Mrs. or Miss If so remove those.
-            if (user.FullName.Contains("Prof.") || user.FullName.Contains("Dr.") || user.FullName.Contains("Mr.") || user.FullName.Contains("Mrs.") || user.FullName.Contains("Miss"))
+            if (user.FullName.Contains("Prof.") || user.FullName.Contains("Dr.") || user.FullName.Contains("Mr.") || user.FullName.Contains("Mrs.") || user.FullName.Contains("Miss") || user.FullName.Contains("Ms"))
             {
                 user.FirstName = user.FullName.Split(' ')[1];
-                user.LastName = user.FullName.Split(' ')[2];
+                // If FullName string contains more then 2 strings concatinate LastName
+                if (user.FullName.Length > 3)
+                {
+                    String message = String.Format("FullName {0} contains more than 1 LastName {1}.", user.FullName, user.FullName.Split(" ").Length);
+                    //Console.WriteLine(message);
+                    int i = (user.FullName.Split(" ").Length);
+                    user.LastName = string.Concat(user.FullName.Split(' ')[2..i]);
+                }
+                else
+                {
+                    user.LastName = user.FullName.Split(' ')[2];
+                }
+                
             }
             else
             {
                 user.FirstName = user.FullName.Split(' ')[0];
-                user.LastName = user.FullName.Split(' ')[1];
+                if (user.FullName.Length > 2)
+                {
+                    int i = (user.FullName.Split(" ").Length);
+                    user.LastName = string.Concat(user.FullName.Split(' ')[1..i]);
+                }
+                else
+                {
+                    user.LastName = user.FullName.Split(' ')[1];
+                }
+                
             };
             
             return user;
