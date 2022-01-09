4b363d94072c127b0ff3a720a34fc85565f83d35
stefanstranger <stefan@stranger.nl>
2022-01-09 14:40:09 +0100

    Added arguments

--- Projects/Faker/Faker/Program.cs
+++ Projects/Faker/Faker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Faker;
 using Faker.Resources;
 using Newtonsoft.Json;
@@ -7,57 +8,85 @@ namespace FakerDemo
 {    
     internal class Program
     {
+        
         class User
         {
             public string FullName { get; set; }
             public string FirstName { get; set; }
             public string LastName { get; set; }
-        }
+        }        
+
         static void Main(string[] args)
         {
+
             string json = JsonConvert.SerializeObject(CreateUser());
-            Console.WriteLine(json);
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine(json);
+                return;
+            }
+            var command = args[0];                    
+
+            switch (command)
+            {
+                case "json":
+                    Console.WriteLine(json);
+                    break;
+                case "table":
+                    dynamic table = JsonConvert.DeserializeObject(json);
+                    Console.WriteLine($"FullName: {table.FullName} FirstName: {table.FirstName} LastName: {table.LastName}");
+                    break;
+                default:
+                    Console.WriteLine(json);
+                    break;
+            }
         }
 
-        private static User CreateUser()
-        {
-            User user = new User();
-            user.FullName = Faker.Name.FullName();
-            // Check if FullName contains Prof, Dr. Mr. Mrs. or Miss If so remove those.
-            if (user.FullName.Contains("Prof.") || user.FullName.Contains("Dr.") || user.FullName.Contains("Mr.") || user.FullName.Contains("Mrs.") || user.FullName.Contains("Miss") || user.FullName.Contains("Ms"))
+        private static object CreateUser()      {
+            var fullNameInput = Faker.Name.FullName();
+
+            var titles = new[]
             {
-                user.FirstName = user.FullName.Split(' ')[1];
-                // If FullName string contains more then 2 strings concatinate LastName
-                if (user.FullName.Length > 3)
-                {
-                    String message = String.Format("FullName {0} contains more than 1 LastName {1}.", user.FullName, user.FullName.Split(" ").Length);
-                    int i = (user.FullName.Split(" ").Length);
-                    user.LastName = string.Concat(user.FullName.Split(' ')[2..i]);
-                }
-                else
-                {
-                    user.LastName = user.FullName.Split(' ')[2];
-                }
-                
-            }
-            else
-            {
-                user.FirstName = user.FullName.Split(' ')[0];
-                if (user.FullName.Length > 2)
-                {
-                    int i = (user.FullName.Split(" ").Length);
-                    user.LastName = string.Concat(user.FullName.Split(' ')[1..i]);
-                }
-                else
-                {
-                    user.LastName = user.FullName.Split(' ')[1];
-                }
-                
+                "Prof",
+                "Dr",
+                "Mr",
+                "Mrs",
+                "Miss",
+                "Ms",
             };
-            
+
+            var index = 0;
+            foreach (var title in titles.OrderByDescending(x => x.Length))
+            {
+                if (!fullNameInput.StartsWith(title))
+                {
+                    continue;
+                }
+
+                switch (fullNameInput[title.Length])
+                {
+                    case ' ':
+                        index = title.Length + 1;
+                        break;
+                    case '.':
+                        index = title.Length + 2;
+                        break;
+                    default:
+                        continue;
+                }
+
+                break;
+            }
+
+            var names = fullNameInput[index..].Split(' ');
+            var user = new User
+            {
+                FullName = fullNameInput,
+                FirstName = names.First(),
+                LastName = string.Join(' ', names.Skip(1))
+            };
             return user;
         }
-    }
-
-    
+    }    
 }
